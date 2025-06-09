3d3fd2a005dcb57516691a909ca5a025d2e6929b
pabloajimenezc <pablo.jimenezc@sansano.usm.cl>
2025-06-09 00:39:29 -0400

    09/06/2025 v2

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import subprocess
 import sys
 
-packages_list = ['Pillow', 'matplotlib', 'numpy']
+packages_list = ['Pillow', 'matplotlib', 'numpy', 'seaborn', 'pandas']
 
 for package in packages_list:
     print(f"Installing {package}...")
